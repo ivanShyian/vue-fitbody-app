e50cacbfd7e82e5397f2e69d542b02bf20f25db6
vanjkes <shyian.ivan@yahoo.com>
2021-01-29 21:29:00 +0200

    Changed home-bar styles for correct view

--- src/components/home/HomeBar.vue
+++ src/components/home/HomeBar.vue
@@ -44,56 +44,87 @@ export default {
   background-color: #abbd81;
   box-shadow: -1px 7px 39px -16px rgba(0, 0, 0, 0.75);
   margin: .5rem 0 .5rem .5rem;
+  padding: 1rem;
+  overflow-y: hidden;
 }
+
 .bar-upper {
   display: flex;
   justify-content: space-between;
   align-items: center;
-  flex: 0 0 40%;
+  flex: 0 1 35%;
   border-bottom: 2px solid rgba(0, 0, 0, .1);
+
+  h2 {
+    @media (max-height: 700px) {
+      font-size: 1.3rem;
+    }
+  }
+
   &__status {
     font-size: 1.2rem;
+
     i {
       color: rgba(0, 0, 0, .5)
     }
+
     span {
       font-weight: 700;
       color: rgba(0, 0, 0, .5);
       font-size: .9rem;
       margin-right: .5rem;
+      @media (max-height: 700px) {
+        font-size: .7rem;
+      }
     }
   }
 }
+
 .bar-bottom {
-  flex: 0 0 60%;
+  flex: 0 0 65%;
   display: flex;
   flex-direction: column;
   justify-content: flex-end;
   padding: 0 1rem;
+
   &__pointers {
     display: flex;
     flex-direction: column;
-    margin: .5rem 0 0 0;
+
     span {
       font-family: "Quicksand", sans-serif;
     }
   }
+
   &__pointers-title {
     font-weight: 700;
     font-size: 1.1rem;
     color: rgba(0, 0, 0, .7);
     text-align: center;
+    @media (max-height: 700px) {
+      font-size: .8rem;
+    }
   }
+
   &__pointers-inner {
     display: flex;
     justify-content: space-between;
+
     span {
       font-weight: 700;
+      @media (max-height: 700px) {
+        font-size: .8rem;
+      }
     }
+
     small {
       font-size: .7rem;
+      @media (max-height: 700px) {
+        font-size: .6rem;
+      }
     }
   }
+
   &__headway-line {
     display: flex;
     align-items: center;
@@ -103,13 +134,19 @@ export default {
     text-align: center;
     background-color: rgba(255, 255, 255, .1);
     border-radius: 1rem;
+    @media (max-height: 700px) {
+      font-size: .8rem;
+    }
+
     span {
       z-index: 2;
       font-weight: 700;
     }
+
     i {
       z-index: 2;
     }
+
     &_active {
       content: '';
       position: absolute;
@@ -118,6 +155,9 @@ export default {
       height: 1.5rem;
       background-color: rgba(100, 255, 100, .7);
       border-radius: 1rem;
+      @media (max-height: 700px) {
+        height: 1.1rem;
+      }
     }
   }
 }
